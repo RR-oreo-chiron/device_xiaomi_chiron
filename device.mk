d9bec094d53faee24ee2d4bc52e1f59306a998a9
Jorge Martin Espinosa <angel.arasthel@gmail.com>
2018-03-24 14:09:35 +0100

    Fix audio, proximity issue and USB
    Change-Id: I6c906c228a8245bfaf7e90bb74bffc86ad04e7fa

--- device.mk
+++ device.mk
@@ -131,7 +131,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    $(LOCAL_PATH)/configs/audio/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # Prebuilt
 PRODUCT_COPY_FILES += \
