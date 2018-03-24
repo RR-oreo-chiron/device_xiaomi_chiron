0537129e1c79574b10bca293fba2074a4083dc6c
Jorge Martin Espinosa <angel.arasthel@gmail.com>
2018-03-24 14:15:43 +0100

    Add shim so newer camera blobs work with libMiCameraHal
    Change-Id: I3da2e18899bdf7ade51be191e989fe16d6744275

--- camera_shim/camera.c
+++ camera_shim/camera.c
@@ -1,3 +1,4 @@
 extern void _ZNK8SkBitmap12unlockPixelsEv() {}
 extern void _ZNK8SkBitmap10lockPixelsEv() {}
 extern void _ZN7SkCodec9getPixelsERK11SkImageInfoPvj() {}
+extern void _ZN7android19QCameraHalInterface15onSetParametersEbiiiiiiiiiiiPcS1_iii() {}
